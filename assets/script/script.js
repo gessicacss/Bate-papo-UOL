907fae3193e82382a24fc6a4cca9d957c2517497
Gessica <santos.gessicacarolina@gmail.com>
2023-01-20 15:40:26 -0300

    adicionando função que mostra as mensagens enviadas na tela e atualiza as mensagens a cada 3 segundos

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,6 +19,7 @@ function loggedIn (response){
     document.querySelector('.login-container').classList.add('hidden');
 
     setInterval(statusUser, 5000);
+    setInterval (getMessage, 3000);
 }
 
 function loginFailed (error){
@@ -53,4 +54,48 @@ function login() {
     const loginUser = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: username})
     loginUser.then (loggedIn);
     loginUser.catch(loginFailed);
+}
+
+//messages functions
+function showMessages (messages){
+
+    for (let i= 0; i < messages.data.length; i++){
+        let from = messages.data[i].from;
+		let to = messages.data[i].to;
+		let text = messages.data[i].text;
+		let type = messages.data[i].type;
+		let time = messages.data[i].time;
+
+        let messagesSent = document.querySelector('main');
+
+        if (type === 'status') {
+            messagesSent.innerHTML += 
+            `<div class="message status"><p><span class="time">(${time})</span> 
+            <span>${from}</span> ${text}</p></div>`;
+        }
+        if (type === 'message') {
+            messagesSent.innerHTML += 
+            `<div class="message public"><p><span class="time">(${time})</span> <span>${from} 
+            </span>para<span> ${to}:</span> ${text}</p></div>`;
+        } else if (type === 'private_message' && (to === username || from === username)) {
+            messagesSent.innerHTML += `
+            <div class="message private"> <p><span class="time">(${time})</span> 
+         <span>${from} </span>reservadamente para<span> ${to}:</span> ${text}</p></div>`;
+        }
+    }
+
+    const showLastMessage = document.querySelector('.message:last-child');
+    showLastMessage.scrollIntoView();
+}
+
+
+function errorInShowingMessages(errorMsg){
+    console.log(errorMsg)
+}
+
+function getMessage () {
+    const getMessages = axios.get ('https://mock-api.driven.com.br/api/v6/uol/messages');
+
+    getMessages.then(showMessages);
+    getMessages.catch(errorInShowingMessages);
 }
